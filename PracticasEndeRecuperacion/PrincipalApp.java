a5f80a5b2d8d1154b48a10049feabd50048113c5
Torrijoss <127211463+Torrijoss@users.noreply.github.com>
2025-06-04 16:12:53 +0200

    Creacion de la PrincipalApp en el main

---
+++ PracticasEndeRecuperacion/PrincipalApp.java
@@ -0,0 +1,27 @@
+public class PrincipalApp {
+static Scanner scan = new Scanner (System.in);
+	
+    public static void main(String[] args) {
+    	
+    	String frase = "";
+    	System.out.println("Introduce la frase");
+    	frase = scan.next();
+    	
+        NumeroConsonantes frases = new NumeroConsonantes(frase);
+        int numcons = frases.contarConsonantes();
+        System.out.println("Número de consonantes: " + numcons);
+        
+	    int a,b,c,d,e,f;
+	    System.out.println("Introduce 6 edades");
+	    a = scan.nextInt();
+	    b = scan.nextInt();
+	    c = scan.nextInt();
+	    d = scan.nextInt();
+	    e = scan.nextInt();
+	    f = scan.nextInt();
+	    int[] edades = {a, b, c, d, e, f};
+	    ContarMayoresEdad contador = new ContarMayoresEdad(edades);
+	    String resultado = contador.contarMayoresDeEdad();
+	    System.out.println(resultado);
+	}
+}
